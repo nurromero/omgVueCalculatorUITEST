5734434a4010dd8e6a776ed9c8fc0a7e969c7d3f
Nuriye <nuriyeerdogan38@gmail.com>
2024-03-11 20:31:28 +0100

    Add project files.

---
+++ omgVueCalculatorUITEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("omgVueCalculatorUITEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("omgVueCalculatorUITEST")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33d5a4c1-7857-4b46-a158-7c51c00d46c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ omgVueCalculatorUITEST/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace vueCalculatorUITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private static readonly string DriverDirectory = "C:\\Users\\baddi\\OneDrive\\Skrivebord\\Programmering - 3rd semester\\webDrivers";
+        private IWebDriver driver;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            driver = new ChromeDriver(DriverDirectory);
+        }
+
+        [TestMethod]
+        public void TestWebsite()
+        {
+            //Navigating to the website
+            driver.Navigate().GoToUrl("https://nursvuecalculator.azurewebsites.net/");
+
+            //Verifying the title of the page
+            Assert.AreEqual("Document", driver.Title);
+
+            //Finding the elements on the page
+            IWebElement num1Element = driver.FindElement(By.CssSelector("input[type='number']:nth-of-type(1)"));
+            num1Element.SendKeys("45");
+
+            IWebElement num2Element = driver.FindElement(By.CssSelector("input[type='number']:nth-of-type(2)"));
+            num2Element.SendKeys("20");
+
+            IWebElement selectElement = driver.FindElement(By.CssSelector("select"));
+
+            //Finding the button and clicking it 
+            IWebElement buttonElement = driver.FindElement(By.CssSelector("button.btn-primary"));
+            buttonElement.Click();
+
+            //Find the result element, verifying/asserting it 
+            //Using last.child, because the result is the last element in the result-container (D:) 
+            IWebElement resultElement = driver.FindElement(By.CssSelector(".result-container > div:last-child"));
+            Assert.AreEqual("Result: 65", resultElement.Text);
+
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Quit();
+        }
+
+
+
+
+    }
+}
